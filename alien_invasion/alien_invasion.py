c3612689dfeea5f54ccfbf612be773a88d1fbd7b
ztq <zhangtongquan1013@126.com>
2019-07-17 14:03:40 +0800

    add files

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -14,15 +14,19 @@ def run_game():
 	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height));
 	pygame.display.set_caption("Alien Invasion");
 	
-	#创建一艘飞船
+	#创建一艘飞船，一个子弹编组和一个外星人编组
 	ship = Ship(ai_settings, screen);
 	bullets = Group()
+	aliens = Group()
+	
+	# 创建外星人群
+	gf.create_fleet(ai_settings, screen, ship, aliens)
 	
 	
 	while True:
 		gf.check_event(ai_settings, screen, ship, bullets);
 		ship.update();
 		gf.update_bullets(bullets)
-		gf.update_screen(ai_settings, screen, ship, bullets);
+		gf.update_screen(ai_settings, screen, ship, aliens, bullets);
 
 run_game()
